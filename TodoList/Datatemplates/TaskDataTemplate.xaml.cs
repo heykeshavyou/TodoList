9feea5f1868980607ac371acc91167cc1e9a5295
Keshav <Keshav210305@gmail.com>
2025-09-30 21:56:01 +0530

    changes

---
+++ TodoList/Datatemplates/TaskDataTemplate.xaml.cs
@@ -0,0 +1,14 @@
+namespace TodoList.Datatemplates;
+
+public partial class TaskDataTemplate : DataTemplate
+{
+	public TaskDataTemplate()
+	{
+		InitializeComponent();
+	}
+
+    private void Grid_BindingContextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ TodoList/Datatemplates/TemplateSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Datatemplates
+{
+    public class TemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return new TaskDataTemplate();
+        }
+    }
+}
