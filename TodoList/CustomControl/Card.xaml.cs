18de52d7e6a05fb063b7b9465398f9363bcd7e37
Keshav <Keshav210305@gmail.com>
2025-10-01 23:55:05 +0530

    shift to custom control for todo card

---
+++ TodoList/CustomControl/Card.xaml.cs
@@ -0,0 +1,52 @@
+using System.Windows.Input;
+using TodoList.Models;
+
+namespace TodoList.CustomControl;
+
+public partial class Card : ContentView
+{
+    public Card()
+    {
+        InitializeComponent();
+    }
+    protected override void OnBindingContextChanged()
+    {
+        var a = BindingContext;
+    }
+    private static BindableProperty TodoProperty = BindableProperty.Create(
+        nameof(Todo),
+        typeof(TodoItem),
+        typeof(Card));
+    public TodoItem Todo
+    {
+        get => (TodoItem)GetValue(TodoProperty);
+        set => SetValue(TodoProperty, value);
+    }
+    private static BindableProperty MarkCommandProperty = BindableProperty.Create(
+        nameof(MarkCommand),
+        typeof(ICommand),
+        typeof(Card));
+    public ICommand MarkCommand
+    {
+        get => (ICommand)GetValue(MarkCommandProperty);
+        set => SetValue(MarkCommandProperty, value);
+    }
+    private static BindableProperty DeleteCommandProperty = BindableProperty.Create(
+        nameof(DeleteCommand),
+        typeof(ICommand),
+        typeof(Card));
+    public ICommand DeleteCommand
+        {
+        get => (ICommand)GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+    public static BindableProperty EditCommandProperty = BindableProperty.Create(
+        nameof(EditCommand),
+        typeof(ICommand),
+        typeof(Card));
+    public ICommand EditCommand
+        {
+        get => (ICommand)GetValue(EditCommandProperty);
+        set => SetValue(EditCommandProperty, value);
+    }
+}
